91eca84a7b39fb051a0d729c39503650507661fd
DAC125 <dcapro170_@estudiantec.cr>
2022-01-22 00:40:33 -0600

    pass document base64

--- src/SEVER/server.py
+++ src/SEVER/server.py
@@ -1,5 +1,7 @@
 from flask import Flask,jsonify,request,json
 from flask.wrappers import Response
+import base64
+import os
 #from speech_to_text_cloud_v import main
 
 app = Flask(__name__)
@@ -9,10 +11,16 @@ app = Flask(__name__)
 def members():
     if request.method == 'POST':
         data = json.loads(request.data)
+        audio_file = base64.b64decode(data['file'])
+        with open(os.path.join(r'C:\Users\dcapr\OneDrive\Documents\SpeechToText\src\Resourses\test', 'aaaa.m4a'), 'wb') as file_to_write:
+            file_to_write.write(audio_file)
+            file_to_write.close()
+        #print(data)
+        '''
         #main(data['nombre'],data['direccion'])
         print("----------------------------------------------------")
         print("El nombre del archivo es: ", data['nombre'])
-        print("----------------------------------------------------")
+        print("----------------------------------------------------")'''
         data = {"members": ["Maria","dac","Alberto"]}
         return jsonify(data)
 
--- src/src/components/content.js
+++ src/src/components/content.js
@@ -1,49 +1,9 @@
 import React, { Component, useState } from 'react'
+import axios from 'axios';
 import '../assets/css/components/Content.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Form, Button } from 'react-bootstrap'
 
-/*
-class Content extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            file: ''
-        }
-    }
-    const [file, setFile] = useState();
-
-
-    onChange(e){
-        let files = e.target.files;
-
-        let reader =  new FileReader();
-        reader.readAsDataURL(files[0]);
-        reader.onload=(e)=>{
-            this.setState({[e.target.name]: e.target.result})
-            console.warn("file data",e.target.result);
-        }
-    };
-
-    render(){
-        return (
-            <Container>
-
-            <div >
-
-
-                <Form.Group controlId="formFile" className="mb-3" method="post" action="/members">
-                    <Form.Label>Seleccione el archivo Mp3 a trancribir</Form.Label>
-                    <Form.Control type="file" onChange={(e)=>this.onChange(e)}/>
-                </Form.Group>
-
-
-            </div>
-        </Container>
-        )
-    }
-}
-export default Content;*/
 
 function Content() {
     const [file, setFile] = useState({
@@ -51,16 +11,29 @@ function Content() {
         file: ''
     });
 
+    const onSuubmit = () => {
+        axios.post("/members",file)
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
     const onSubmit = () => {
-        console.log(file.fileUrl)
+        console.log(file)
         let reader =  new FileReader();
         reader.readAsDataURL(file.fileUrl[0]);
         reader.onload=(e)=>{
             console.log(e.target.result)
-            /*setFile({
-                ...file
-            [e.target.name]: e.target.result})*/
+            setFile({
+                ...file,
+            file: e.target.result})
         }
+       
+
+        
 
     };
 
@@ -70,13 +43,10 @@ function Content() {
             ...file,
             [event.target.name]: event.target.files
         })
+        console.log(file)
     };
 
-    /*const handleChange(event) {
-        let fieldName = event.target.name;
-        let fleldVal = event.target.value;
-        this.setState({form: {...this.state.form, [fieldName]: fleldVal}})
-      }*/
+  
 
     return (
         
@@ -87,6 +57,7 @@ function Content() {
                     <Form.Label>Seleccione el archivo Mp3 a trancribir</Form.Label>
                     <Form.Control type="file" onChange={handleChange} name="fileUrl"/>
                     <Button variant="primary" type="submit" onClick={onSubmit}>submit</Button>
+                    <Button variant="primary" type="submit" onClick={onSuubmit}>submit</Button>
                 </Form.Group>
 
 
