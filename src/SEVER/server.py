0d194b94de1b7cae2d10493ec9bcff0c3a4de50c
DAC125 <dcapro170_@estudiantec.cr>
2022-01-27 22:56:42 -0600

    pass txt base64

--- src/SEVER/server.py
+++ src/SEVER/server.py
@@ -2,7 +2,6 @@ from flask import Flask,jsonify,request,json
 from flask.wrappers import Response
 import base64
 import os
-import time
 #from speech_to_text_cloud_v import main
 
 app = Flask(__name__)
@@ -11,25 +10,24 @@ app = Flask(__name__)
 @app.route("/members", methods=["POST"])
 def members():
     if request.method == 'POST':
-        print("waiting...")
-        '''
         print(request)
         req = json.loads(request.data)
         #print(req)
-        
+        '''
         audio_file = base64.b64decode(req['fileBase64'])
         with open(os.path.join('/home/dac125/Documents/SpeechToText/src/Resourses/test', req['name']), 'wb') as file_to_write:
             file_to_write.write(audio_file)
             file_to_write.close()
         #print(req)
-        
+        '''
+        '''
         #main(req['nombre'],req['direccion'])
         print("----------------------------------------------------")
         print("El nombre del archivo es: ", req['nombre'])
-        print("----------------------------------------------------")
-        '''
-        time.sleep(10)
-        res = {"members": ["Maria","dac","Alberto"]}
+        print("----------------------------------------------------")'''
+        with open("/home/dac125/Documents/SpeechToText/src/Resourses/Transcription/messi.txt", "rb") as file:
+            encoded_string = base64.b64encode(file.read())
+        res = {"members": encoded_string.decode()}
         return jsonify(res)
 
 
